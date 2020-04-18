c8fdd4d359023616cd0d479677459cb942dcf719
pavan <dongare.pavan25@gmail.com>
2020-04-19 03:51:05 +0530

    fetch data from firebase

--- evernote-tutorial/src/App.js
+++ evernote-tutorial/src/App.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-
+const firebase = require('firebase');
 
 
 
 
 
 class App extends React.Component {
+  
+  constructor(){
+    super();
+    this.state = {
+      selectedNoteIndex:null,
+      selectedNote:null,
+      note:null
+    }
+  }
+
+
+
   render(){
     return(<div> Hello world </div> );
   }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection('notes')
+      .onSnapshot( serverUpdate =>{
+        const notes = serverUpdate.docs.map( _doc=> {
+          const data = _doc.data();
+          data['id'] = _doc.id;
+          return data;
+        });
+        console.log(notes);
+        this.setState({notes:notes});
+      });
+
+  }
+
+
 }
 
 export default App;
+ 
